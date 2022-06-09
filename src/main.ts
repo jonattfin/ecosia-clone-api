6577a077d826fd3c9586e55bad92b6239e5bbf2a
jonattfin <jonattfin@outlook.com>
2022-06-09 11:13:51 +0300

    added the projects module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Ecosia-Clone API')
+    // .setDescription('The cats API description')
+    .setVersion('1.0')
+    // .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
