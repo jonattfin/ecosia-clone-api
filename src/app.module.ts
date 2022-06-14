555fb4933f33d6a68d37347be8397ff989c6a0a6
jonattfin <jonattfin@outlook.com>
2022-06-14 16:39:28 +0300

    wip

--- src/app.module.ts
+++ src/app.module.ts
@@ -26,22 +26,12 @@ import { ReportsModule } from './reports/reports.module';
 export class AppModule {}
 
 function getConnection() {
-  const remoteConnection = {
-    host: 'oliadkuxrl9xdugh.chr7pe7iynqr.eu-west-1.rds.amazonaws.com',
-    username: 'nq5h2x8la1nxva90',
-    password: 'xjl43fm5omgfojqk',
-    database: 'bmg6kkfdmf4wgnwe',
+  const herokuConnection = {
+    host: process.env.DB_HOST,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
   };
 
-
-  // const herokuConnection = {
-  //   host: process.env.DB_HOST,
-  //   username: process.env.DB_USERNAME,
-  //   password: process.env.DB_PASSWORD,
-  //   database: process.env.DB_DATABASE,
-  // };
-
-  // return herokuConnection;
-
-  return remoteConnection;
+  return herokuConnection;
 }
