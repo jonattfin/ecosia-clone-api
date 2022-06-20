ceb3f820d7d06411920936c6e76974690ae0984b
jonattfin <jonattfin@outlook.com>
2022-06-20 12:57:34 +0300

    small changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -54,20 +54,11 @@ import { HealthModule } from './health/health.module';
 })
 export class AppModule {}
 
-// function getConnection() {
-//   return {
-//     host: process.env.DB_HOST,
-//     username: process.env.DB_USERNAME,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_DATABASE,
-//   };
-// }
-
 function getConnection() {
   return {
-    host: 'oliadkuxrl9xdugh.chr7pe7iynqr.eu-west-1.rds.amazonaws.com',
-    username: 'nq5h2x8la1nxva90',
-    password: 'xjl43fm5omgfojqk',
-    database: 'bmg6kkfdmf4wgnwe',
+    host: process.env.DB_HOST,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
   };
 }
